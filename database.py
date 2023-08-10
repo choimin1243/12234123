77922c7ff14bf968b0dfdb81bbbcdae472ea8f5c
yeonsama <62858055+choimin1243@users.noreply.github.com>
2023-08-11 06:00:31 +0900

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:test1234@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL='postgresql://qhceykvr:VcmJgJKWHEw9QMxmdjRRK_zUBmU97_GD@satao.db.elephantsql.com/qhceykvr'
 
 
 
